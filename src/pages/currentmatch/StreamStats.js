7da5f0257810a8a9dc13960df1d4a02fd57b7849
ShidemantleJ <jcshidemantle@crimson.ua.edu>
2024-11-22 22:42:42 -0600

    fix stream stats

--- src/pages/currentmatch/StreamStats.js
+++ src/pages/currentmatch/StreamStats.js
@@ -41,27 +41,41 @@ function StreamStats () {
       return <h1 style={{marginTop: '3px'}}>{team2.teamName}</h1>
     case "team1times":
       return (
-        <div>
-        {[...team1.times[team1.currentIndex]].reverse().map((time, index) => (
-          <h1 style={{backgroundColor: '#bdbdbd', fontWeight: '600', marginBottom: '0px', marginTop: '0px'}} key={index}>{time}</h1>
-        ))}
+        <div style={{backgroundColor: "#bdbdbd", width: "100vw", height: "100vh"}}>
+          {team1?.times?.[team1?.currentIndex]?.length > 0 && !team1.times[team1.currentIndex].isEmpty && 
+            [...team1.times[team1.currentIndex]].reverse().map((time, index) => (
+              <h1 style={{fontWeight: '600', marginBottom: '0px', marginTop: '0px'}} key={index}>{time}</h1>
+            ))
+          }
         </div>
       )
     case "team2times":
       return (
-          [...team2.times[team2.currentIndex]].reverse().map((time, index) => (
-            <h1 style={{backgroundColor: '#bdbdbd', fontWeight: '600', marginBottom: '0px', marginTop: '0px'}} key={index}>{time}</h1>
-          ))
-        )
+        <div style={{backgroundColor: "#bdbdbd", width: "100vw", height: "100vh"}}>
+          {team2?.times?.[team2?.currentIndex]?.length > 0 && 
+            [...team2.times[team2.currentIndex]].reverse().map((time, index) => (
+              <h1 style={{fontWeight: '600', marginBottom: '0px', marginTop: '0px'}} key={index}>{time}</h1>
+            ))
+          }
+        </div>
+      )
     case "team1playername":
       return <h1 style={{position: 'absolute', bottom: 0, left: '50%', transform: 'translateX(-50%)', marginBottom: '25px'}}>{team1.names[team1.currentIndex]}</h1>
     case "team2playername":
       return <h1 style={{position: 'absolute', bottom: 0, left: '50%', transform: 'translateX(-50%)', marginBottom: '25px'}}>{team2.names[team2.currentIndex]}</h1>
     case "team1recenttime":
-      return <h1 style={{position: 'absolute', bottom: 0, left: '50%', transform: 'translateX(-50%)', marginBottom: '25px'}}>{team1.times[team1.currentIndex][team1.times[team1.currentIndex].length - 1]}</h1>
+      return (
+        <h1 style={{position: 'absolute', bottom: 0, left: '50%', transform: 'translateX(-50%)', marginBottom: '25px'}}>
+          {team1?.times?.[team1?.currentIndex]?.length > 0 ? team1.times[team1.currentIndex][team1.times[team1.currentIndex].length - 1] : ''}
+        </h1>
+      )
     case "team2recenttime":
-      return <h1 style={{position: 'absolute', bottom: 0, left: '50%', transform: 'translateX(-50%)', marginBottom: '25px'}}>{team2.times[team2.currentIndex][team2.times[team2.currentIndex].length - 1]}</h1>
-    default:
+      return (
+        <h1 style={{position: 'absolute', bottom: 0, left: '50%', transform: 'translateX(-50%)', marginBottom: '25px'}}>
+          {team2?.times?.[team2?.currentIndex]?.length > 0 ? team2.times[team2.currentIndex][team2.times[team2.currentIndex].length - 1] : ''}
+        </h1>
+      )
+      default:
       return <div>Invalid element</div>;
   }
 };

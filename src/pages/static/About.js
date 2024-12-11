8fdc1b10da3f18f4f1d4b3889ae53fcd58b34e13
ShidemantleJ <jcshidemantle@crimson.ua.edu>
2024-12-11 15:27:13 -0600

    changed to correct number of teams

--- src/pages/static/About.js
+++ src/pages/static/About.js
@@ -1,20 +1,37 @@
 // pages/About.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Box, Paper, Typography } from '@mui/material';
-import './Home.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Box, Paper, Typography } from "@mui/material";
+import "./Home.css";
 
 const About = () => {
   return (
-    <Box sx={{ backgroundColor: '#f5f5f5', padding: 4, minHeight: '100vh' }}>
-      <Paper elevation={3} sx={{ padding: 4, maxWidth: 800, margin: 'auto', textAlign: "center" }}>
-        <Typography variant="h3" sx={{ mb: 3 }}>About the Competition</Typography>
-        <Typography variant="body1" sx={{ mb: 4 }}>The Collegiate Cubing Championship is a project from the University of Alabama's Cubing Club, Alabama Crimson Cubing. Starting in January, matches will be organized between teams comprised of 3 members of each of 32 college cubing clubs across the United States.</Typography>
-        <Typography variant="h3" sx={{ mb: 3 }}>About the Organizers</Typography>
-        <Typography variant="body1">The organizers of this competition are Alex Guirgues, Martin Albright, and John Shidemantle. For inquiries relating to the competition, please <Link to="/contact">contact us</Link></Typography>
+    <Box sx={{ backgroundColor: "#f5f5f5", padding: 4, minHeight: "100vh" }}>
+      <Paper
+        elevation={3}
+        sx={{ padding: 4, maxWidth: 800, margin: "auto", textAlign: "center" }}
+      >
+        <Typography variant="h3" sx={{ mb: 3 }}>
+          About the Competition
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 4 }}>
+          The Collegiate Cubing Championship is a project from the University of
+          Alabama's Cubing Club, Alabama Crimson Cubing. Starting in January,
+          matches will be organized between teams comprised of 3 members each
+          from 28 college cubing clubs across the United States.
+        </Typography>
+        <Typography variant="h3" sx={{ mb: 3 }}>
+          About the Organizers
+        </Typography>
+        <Typography variant="body1">
+          The organizers of this competition are Alex Guirgues, Martin Albright,
+          and John Shidemantle. For inquiries relating to the competition,
+          please <Link to="/contact">contact us</Link> at
+          crimsoncubingorganizers@gmail.com
+        </Typography>
       </Paper>
     </Box>
   );
-}
+};
 
 export default About;
